c6d960f07ad0cfc7dee5e5816cf3b19f53b790bf
Denis Kalyuzhnyy <denis.kaluzhnyy@gmail.com>
2020-06-30 11:01:30 +0300

    [+] hello world tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,35 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_root_configfile(host):
+    f = host.file('/etc/systemd/system/test.service')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_root_service_started(host):
+    f = host.file('/var/log/root_test.log')
+
+    assert f.content_string == 'Hello, Root World'
+
+def test_user_configfile(host):
+    f = host.file('/root/.config/system/test.service')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+def test_user_service_started(host):
+    f = host.file('/var/log/user_test.log')
+
+    assert f.content_string == 'Hello, User World'
+
+def test_user_configfile(host):
+    f = host.file('/root/.config/system/user_test.service')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
